ff17361bfe77113adbad9527ced77a33b5b7cde9
Alex Abatov <a.aliksant315@gmail.com>
2024-11-26 17:23:47 +0900

    POST requests

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const port = 5001;
 const app = express()
 
+//body parser middleware
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.get('/',(req, res) => {
     res.json({message: `Welcome to the RandomIdeas API`})
 })
